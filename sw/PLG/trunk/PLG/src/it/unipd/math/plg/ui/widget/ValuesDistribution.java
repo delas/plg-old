fec058e96d1aec36b56d17d10a66446c336689b9
delas <delas@a6f00bd5-ff82-0410-82e6-d0698ff6f273>
2010-07-10 18:15:20 +0000

    - Updated the interface in order to use the new version of the library (with probability distributions among split branches)

---
+++ sw/PLG/trunk/PLG/src/it/unipd/math/plg/ui/widget/ValuesDistribution.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.unipd.math.plg.ui.widget;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author delas
+ */
+public class ValuesDistribution extends JComponent {
+
+	protected void paintComponent(Graphics g) {
+		int width = getWidth();
+		int height = getHeight();
+
+		Graphics2D g2d = (Graphics2D)g;
+	}
+}
