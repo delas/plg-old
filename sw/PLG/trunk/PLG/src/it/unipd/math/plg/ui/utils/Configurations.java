0362244572abb04d057e56e05bb8d975e4701db0
delas <delas@a6f00bd5-ff82-0410-82e6-d0698ff6f273>
2011-08-27 21:09:17 +0000

    - Added representation of processes as Petri net into the exploration of the hierarchy

--- sw/PLG/trunk/PLG/src/it/unipd/math/plg/ui/utils/Configurations.java
+++ sw/PLG/trunk/PLG/src/it/unipd/math/plg/ui/utils/Configurations.java
@@ -14,7 +14,7 @@ public class Configurations {
 	/** The current software version (string representation) */
 	public static String SW_VERSION = "1.4beta";
 	/** The current software release (digit representation) */
-	public static Double SW_RELEASE = 1.2;
+	public static Double SW_RELEASE = 1.40;
 	/** URL for the web server with updates information */
 	public static String UPDATES_URL = "http://updates.processmining.it/";
 
